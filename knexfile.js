4e27ea78bd7b376e1788353c7aa379c3f02dabbe
vagrant <kennethracadio@hotmail.com>
2018-02-06 21:19:29 +0000

    Migrations - Setup: Connection Configuration

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+const settings = require("./settings.json"); // settings.json
+
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    version: '7.2',
+    connection: {
+      user : settings.user,
+      password : settings.password,
+      database : settings.database
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
