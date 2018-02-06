29b6199d9def7250a3c2b14610f34ceb84461dc6
vagrant <kennethracadio@hotmail.com>
2018-02-06 18:44:46 +0000

    Searching Famous People using Knex

---
+++ lookup_peopleKnex.js
@@ -0,0 +1,26 @@
+const settings = require("./settings.json"); // settings.json
+
+const knex = require('knex')({
+  client: 'pg',
+  version: '7.2',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+const name = process.argv[2];
+
+knex.select('*').from('famous_people')
+.where('first_name', `${name}`)
+.orWhere('last_name', `${name}`)
+.asCallback(function(err, rows) {
+  console.log(`node lookup_people.js ${name}`);
+  console.log("Searching ...");
+  if (err) return console.error(err);
+  console.log(`Found ${rows.length} by the name '${name}':`);
+  console.log(rows);
+});
+
